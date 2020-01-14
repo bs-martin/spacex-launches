cbaecface881ea1b1e6b83239afc3742fc611721
BS Martin <brian@bsmartin.com>
2020-01-14 14:48:53 -0500

    Initial checkin of React template from R&P

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../scss/styles.css'
+
+class App extends Component {
+	render() {
+		return (
+			<div id="app" className="page-wrapper">
+				<h1>SpaceX Launches</h1>
+			</div>
+		)
+	}
+}
+
+export default App
